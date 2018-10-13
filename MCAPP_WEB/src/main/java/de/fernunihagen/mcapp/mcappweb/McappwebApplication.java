b61080d758035fbf7b815338f37b0ab41ba2ab1c
Allan <allan.kaufmann@gmail.com>
2018-10-13 22:48:56 +0200

    Init MCAPP_WEB-Projekt (spring boot)

---
+++ MCAPP_WEB/src/main/java/de/fernunihagen/mcapp/mcappweb/McappwebApplication.java
@@ -0,0 +1,12 @@
+package de.fernunihagen.mcapp.mcappweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McappwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McappwebApplication.class, args);
+	}
+}
