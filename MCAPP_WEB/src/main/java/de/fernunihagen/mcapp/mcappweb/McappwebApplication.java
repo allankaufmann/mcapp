66f76b59b0947bae6312b58fcf536a80fd7dc1c3
Allan <allan.kaufmann@gmail.com>
2018-10-14 21:33:21 +0200

    Testaufruf Thema 1

--- MCAPP_WEB/src/main/java/de/fernunihagen/mcapp/mcappweb/McappwebApplication.java
+++ MCAPP_WEB/src/main/java/de/fernunihagen/mcapp/mcappweb/McappwebApplication.java
@@ -1,10 +1,17 @@
 package de.fernunihagen.mcapp.mcappweb;
 
+import de.fernunihagen.mcapp.mcappweb.domain.Thema;
+import de.fernunihagen.mcapp.mcappweb.repositories.ThemaRepository;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class McappwebApplication {
+@EnableJpaRepositories(basePackageClasses = ThemaRepository.class)
+@EntityScan(basePackageClasses = Thema.class)
+		public class McappwebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(McappwebApplication.class, args);
