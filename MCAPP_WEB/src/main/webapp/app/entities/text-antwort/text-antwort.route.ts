b9998506994bd17b790219b42df62e2da6603ee8
Allan <allan.kaufmann@gmail.com>
2018-11-10 19:48:58 +0100

    Bei Fragen werden nun Textantworten angezeigt und können auch bearbeitet werden. Frage muss nun nicht mehr in Combobox ausgewählt werden.

--- MCAPP_WEB/src/main/webapp/app/entities/text-antwort/text-antwort.route.ts
+++ MCAPP_WEB/src/main/webapp/app/entities/text-antwort/text-antwort.route.ts
@@ -5,23 +5,42 @@ import { UserRouteAccessService } from 'app/core';
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { TextAntwort } from 'app/shared/model/text-antwort.model';
+import {Frage, Fragetyp} from 'app/shared/model/frage.model';
 import { TextAntwortService } from './text-antwort.service';
 import { TextAntwortComponent } from './text-antwort.component';
 import { TextAntwortDetailComponent } from './text-antwort-detail.component';
 import { TextAntwortUpdateComponent } from './text-antwort-update.component';
 import { TextAntwortDeletePopupComponent } from './text-antwort-delete-dialog.component';
 import { ITextAntwort } from 'app/shared/model/text-antwort.model';
+import {FrageService} from "app/entities/frage";
 
 @Injectable({ providedIn: 'root' })
 export class TextAntwortResolve implements Resolve<ITextAntwort> {
-    constructor(private service: TextAntwortService) {}
+    constructor(private service: TextAntwortService, private frageservice: FrageService) {}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const id = route.params['id'] ? route.params['id'] : null;
         if (id) {
             return this.service.find(id).pipe(map((textAntwort: HttpResponse<TextAntwort>) => textAntwort.body));
         }
-        return of(new TextAntwort());
+
+        /*
+         * Wenn für eine bestimmte Frage eine Textantwort erstellt werden soll, dann sollte Frage nicht explizit gesetzt werden
+         * müssen. Aus diesem Grund wird die frageID als queryParameter übergeben.
+         * Da an dieser Stelle ein neues TextAntwort-Objekt erzeugt wird, wird ein Frageobjekt mit der passendne ID
+         * in das Textantwort-Objekt gesetzt. Dabei ist die ID der Frage ausreichend.
+         */
+
+        const frageid = route.queryParams['frageid'] ? route.queryParams['frageid'] : null;
+        var t = new TextAntwort();
+
+        // Prüfen, ob frageID übergeben wurde und numerisch ist
+        if (frageid && !isNaN(frageid)) {
+            let f = new Frage();
+            f.id=Number(frageid);
+            t.frage=f;
+        }
+        return of(t);
     }
 }
 
